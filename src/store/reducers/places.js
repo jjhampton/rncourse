0be092589edefd421e5917de75c7fc5fd8a2c153
John Hampton <john_j_hampton@yahoo.com>
2019-02-23 19:32:52 -0500

    Set up places reducer

--- src/store/reducers/places.js
+++ src/store/reducers/places.js
@@ -1,3 +1,5 @@
+import { ADD_PLACE, DELETE_PLACE, SELECT_PLACE, DESELECT_PLACE } from '../actions/actionTypes';
+
 const initialState = {
     places: [],
     selectedPlace: null
@@ -5,6 +7,37 @@ const initialState = {
 
 const reducer = (state = initialState, action) => {
     switch (action.type) {
+        case ADD_PLACE:
+            return {
+                ...state,
+                places: state.places.concat({
+                    key: Math.random().toString(),
+                    name: action.placeName,
+                    image: {
+                        uri: 'https://picsum.photos/200/300'
+                    }
+                })
+            };
+        case DELETE_PLACE:
+            return {
+                ...state,
+                places: state.places.filter(place => {
+                    return place.key !== state.selectedPlace.key;
+                }),
+                selectedPlace: null
+            };
+        case SELECT_PLACE:
+            return {
+                ...state,
+                selectedPlace: state.places.find(place => {
+                    return place.key === action.placeKey;
+                })
+            };
+        case DELETE_PLACE:
+            return {
+                ...state,
+                selectedPlace: null
+            };
         default:
             return state;
     }
